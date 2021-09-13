ca64e94360027a4283db9ef41115da8c5ec032cc
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2021-09-13 16:07:07 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,129 +5,188 @@ var swiper = new Swiper(".KazanSlider-swiper", {
     },
   });
 
-  var swiper = new Swiper(".OverSlider-swiper", {
+var swiper = new Swiper(".OverSlider-swiper", {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
   });
 
+var swiper = new Swiper(".Accessories-swiper", {
+    navigation: {
+      nextEl: ".Accessories-button-next",
+      prevEl: ".Accessories-button-prev",
+    },
+    pagination: {
+      el: ".Accessories-pagination",
+      clickable: true,
+    },
+  });
+
+var swiper = new Swiper(".Comment-swiper", {
+    navigation: {
+      nextEl: ".Comment-button-next",
+      prevEl: ".Comment-button-prev",
+    },
+    pagination: {
+      el: ".Comment-pagination",
+      clickable: true,
+    },
+  });
+
+
 function btnOverOne() {
   document.getElementById("overBtnOne").classList.add("over__menu__btn--active");
   document.getElementById("overBtnBoxOne").classList.add("over__menu__btn__box--active");
+  document.getElementById("productOne").classList.remove("d-none");
 
   document.getElementById("overBtnTwo").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxTwo").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productTwo").classList.add("d-none");
 
   document.getElementById("overBtnThree").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxThree").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productThree").classList.add("d-none");
 
   document.getElementById("overBtnFour").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxFour").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productFour").classList.add("d-none");
 
   document.getElementById("overBtnFive").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxFive").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productFive").classList.add("d-none");
 
   document.getElementById("overBtnSix").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxSix").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productSix").classList.add("d-none");
 };
 
 function btnOverTwo() {
   document.getElementById("overBtnOne").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxOne").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productOne").classList.add("d-none");
 
   document.getElementById("overBtnTwo").classList.add("over__menu__btn--active");
   document.getElementById("overBtnBoxTwo").classList.add("over__menu__btn__box--active");
+  document.getElementById("productTwo").classList.remove("d-none");
 
   document.getElementById("overBtnThree").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxThree").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productThree").classList.add("d-none");
 
   document.getElementById("overBtnFour").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxFour").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productFour").classList.add("d-none");
 
   document.getElementById("overBtnFive").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxFive").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productFive").classList.add("d-none");
 
   document.getElementById("overBtnSix").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxSix").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productSix").classList.add("d-none");
 };
 
 function btnOverThree() {
   document.getElementById("overBtnOne").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxOne").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productOne").classList.add("d-none");
 
   document.getElementById("overBtnTwo").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxTwo").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productTwo").classList.add("d-none");
 
   document.getElementById("overBtnThree").classList.add("over__menu__btn--active");
   document.getElementById("overBtnBoxThree").classList.add("over__menu__btn__box--active");
+  document.getElementById("productThree").classList.remove("d-none");
 
   document.getElementById("overBtnFour").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxFour").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productFour").classList.add("d-none");
 
   document.getElementById("overBtnFive").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxFive").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productFive").classList.add("d-none");
 
   document.getElementById("overBtnSix").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxSix").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productSix").classList.add("d-none");
 };
 
 function btnOverFour() {
   document.getElementById("overBtnOne").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxOne").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productOne").classList.add("d-none");
 
   document.getElementById("overBtnTwo").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxTwo").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productTwo").classList.add("d-none");
 
   document.getElementById("overBtnThree").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxThree").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productThree").classList.add("d-none");
 
   document.getElementById("overBtnFour").classList.add("over__menu__btn--active");
   document.getElementById("overBtnBoxFour").classList.add("over__menu__btn__box--active");
+  document.getElementById("productFour").classList.remove("d-none");
 
   document.getElementById("overBtnFive").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxFive").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productFive").classList.add("d-none");
 
   document.getElementById("overBtnSix").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxSix").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productSix").classList.add("d-none");
 };
 
 function btnOverFive() {
   document.getElementById("overBtnOne").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxOne").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productOne").classList.add("d-none");
 
   document.getElementById("overBtnTwo").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxTwo").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productTwo").classList.add("d-none");
 
   document.getElementById("overBtnThree").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxThree").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productThree").classList.add("d-none");
 
   document.getElementById("overBtnFour").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxFour").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productFour").classList.add("d-none");
 
   document.getElementById("overBtnFive").classList.add("over__menu__btn--active");
   document.getElementById("overBtnBoxFive").classList.add("over__menu__btn__box--active");
+  document.getElementById("productFive").classList.remove("d-none");
 
   document.getElementById("overBtnSix").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxSix").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productSix").classList.add("d-none");
 };
 
 function btnOverSix() {
   document.getElementById("overBtnOne").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxOne").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productOne").classList.add("d-none");
 
   document.getElementById("overBtnTwo").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxTwo").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productTwo").classList.add("d-none");
 
   document.getElementById("overBtnThree").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxThree").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productThree").classList.add("d-none");
 
   document.getElementById("overBtnFour").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxFour").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productFour").classList.add("d-none");
 
   document.getElementById("overBtnFive").classList.remove("over__menu__btn--active");
   document.getElementById("overBtnBoxFive").classList.remove("over__menu__btn__box--active");
+  document.getElementById("productFive").classList.add("d-none");
 
   document.getElementById("overBtnSix").classList.add("over__menu__btn--active");
   document.getElementById("overBtnBoxSix").classList.add("over__menu__btn__box--active");
+  document.getElementById("productSix").classList.remove("d-none");
 };
